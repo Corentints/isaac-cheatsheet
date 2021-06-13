1384e5d5e05b71e8f7961084d75e02b23e52178a
Gwladys <gwladys.pinel54@gmail.com>
2021-06-13 23:50:12 +0200

    feat(transformation): add data

---
+++ data/transformations.ts
@@ -0,0 +1,109 @@
+export const transformations = [
+  {
+    "id": "5",
+    "name": "Fun Guy",
+    "image": "./images/transformations/transformation_Fun_Guy.png",
+    "description": "\n+1 Red Heart container.\n\n",
+    "items": [ "11", "342", "12", "71", "121", "120", "398", "625", "582", "553" ],
+  },
+  {
+    "id": "12",
+    "name": "Adult",
+    "image": "./images/transformations/transformation_Adult.png",
+    "description": "\nGrants a heart container.\n\n",
+    "requirements": "\nRequires 3 puberty pills"
+  },
+  {
+    "id": "13",
+    "name": "Bookworm",
+    "image": "./images/transformations/transformation_Bookworm.png",
+    "description": "\nRoughly 25% of the time, Isaac shoots an extra tear, like 20/20 item.\n\n",
+    "items" : [ "65", "78", "287", "58", "282", "123", "292", "192", "33", "34", "97", "35", "545", "584", "712" ],
+  },
+  {
+    "id": "9",
+    "name": "Spun",
+    "image": "./images/transformations/transformation_Spun.png",
+    "description": "\nGives +2 damage and +0.15 speed, and spawns a random pill upon transformation.\n\n",
+    "items": [ "240", "70", "14", "143", "345", "13", "493", "496" ],
+  },
+  {
+    "id": "10",
+    "name": "Super Bum",
+    "image": "./images/transformations/transformation_Super_Bum.png",
+    "description": "\nReplaces  Bum Friend,  Dark Bum, and  Key Bum for Super Bum. This bum collects any of the other beggars' pickups, and offers twice the rewards.\n\n",
+    "items": [ "144", "278", "388" ],
+  },
+  {
+    "id": "3",
+    "name": "Bob",
+    "image": "./images/transformations/transformation_Bob.png",
+    "description": "\nIsaac gains a green trail of poisonous creep.\n\n",
+    "items": [ "273", "140", "149", "42" ],
+  },
+  {
+    "id": "7",
+    "name": "Oh Crap",
+    "image": "./images/transformations/transformation_Oh_Crap.png",
+    "description": "\nWhenever a pile of poop is destroyed, health is replenished by half a heart.\n\n",
+    "items": [ "236", "209", "9", "378", "543", "576", "680", "291", "36", "504" ],
+  },
+  {
+    "id": "6",
+    "name": "Leviathan",
+    "image": "./images/transformations/transformation_Leviathan.png",
+    "description": "\nGrants flight and gives +2 black hearts.\n\n",
+    "items": [ "230", "118", "51", "159", "79", "80", "399", "82", "572", "692", "83", "556" ],
+  },
+  {
+    "id": "2",
+    "name": "Guppy",
+    "image": "./images/transformations/transformation_Guppy.png",
+    "description": "\nGrants flight and spawns a Blue Fly 50% of the time a tear hits an enemy.\n\n",
+    "items": [ "81", "212", "187", "134", "665", "145", "133" ],
+    "trinkets": [ "169" ]
+  },
+  {
+    "id": "8",
+    "name": "Seraphim",
+    "image": "./images/transformations/transformation_Seraphim.png",
+    "description": "\nGrants flight and gives +3 soul hearts.\n\n",
+    "items": [ "33", "185", "184", "313", "173", "72", "101", "112", "363", "162", "568", "331", "374", "573", "643", "182", "691", "696", "142" ],
+  },
+  {
+    "id": "11",
+    "name": "Yes Mother?",
+    "image": "./images/transformations/transformation_Yes_Mother.png",
+    "description": "\nIsaac gains a stationary knife that trails directly behind him. It synergizes like  Mom's Knife would.\n\n",
+    "items": [ "195", "110", "55", "200", "30", "199", "114", "31", "228", "139", "29", "217", "355", "508", "102", "39", "41", "439", "552", "604" ],
+  },
+  {
+    "id": "14",
+    "name": "Spider Baby",
+    "image": "./images/transformations/transformation_Spider_Baby.png",
+    "description": "\nSpawns a spider familiar that applies random status effects to enemies it comes in contact with.\n\n",
+    "items": [ "153", "89", "211", "403", "170", "248", "234", "266", "280", "377", "367", "461", "717", "575", "288", "171" ],
+  },
+  {
+    "id": "1",
+    "name": "Beelzebub",
+    "image": "./images/transformations/transformation_Beelzebub.png",
+    "description": "\nGrants flight and converts small enemy flies into Blue Flies.\n\n",
+    "items": [ "320", "272", "274", "279", "57", "128", "10", "248", "148", "9", "264", "151", "364", "365", "426", "430", "511", "629", "649", "581", "693", "461", "492", "434", "650" ],
+  },
+  {
+    "id": "4",
+    "name": "Conjoined",
+    "image": "./images/transformations/transformation_Conjoined.png",
+    "description": "\nWhen attacking, the tumor faces shoot tears diagonally outwards, similar to the effect of  The Wiz. Also gives -0.3 damage and slightly decreases tears.\n\n",
+    "items": [ "8", "167", "269", "100", "322", "268", "67", "188", "113", "265", "163", "112", "275", "174", "95", "267", "404", "361", "360", "431", "390", "363", "491", "518", "472", "607", "652", "608", "679", "661", "698" ],
+  },
+  {
+    "id": "15",
+    "name": "Stompy",
+    "image": "./images/transformations/transformation_Stompy.png",
+    "description": "\nUpon getting three items or pills that increase Isaac's size, he will create dust clouds when walking and have a chance to spawn rock waves around him upon taking damage.\n",
+    "items": [ "302", "12"],
+    "requirements": "\nand One Makes You Larger pill"
+  }
+]
